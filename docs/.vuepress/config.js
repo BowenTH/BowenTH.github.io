a715f7b367bf53d322d8c238503b0a2b4e78dd82
yanyangyang <yanyangyang@koopoo.com.cn>
2018-05-22 01:43:36 +0800

    v1.0

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,16 +5,18 @@ module.exports = {
 	// 	['link', { rel: 'icon', href: '/images/Book.png'}]
 	// ]
 	base: './',
-  title: 'note',
+  title: 'Flex布局',
   description: '文件上传组件 vue文件上传 多文件上传',
   // port:8888,
   dest: 'docs/dist',
   themeConfig: {
-    repo: 'ZhuLiangT/v-alien-upload',
+    repo: 'https://gitlab.com/koopoo/qhw-mp',
     sidebar: [
       ['/flex/', '声明'],
       ['/column/', '横向布局'],
-      // ['/example/', '示例'],
+      ['/row/', '竖向布局'],
+      ['/other/', '其他'],
+      ['/source/', '源码'],
       // ['/getstart/', '起步'],
       // ['/api/', '属性'],
       // ['/function/', '方法']
--- docs/flex/README.md
+++ docs/flex/README.md
@@ -1,4 +1,17 @@
 # flex布局
+> [阮一峰Flex布局教程 传送门](http://www.ruanyifeng.com/blog/2015/07/flex-grammar.html?^%$)
 
-### 1. 要声明高宽
-### 2. 要声明flex布局
+> [github地址](https://github.com/Coffcer/flex-layout)
+
+<br/>
+
+::: tip
+ 1.  一般要声明高宽
+ 2.  要声明flex布局
+:::
+
+<br/>
+<br/>
+<br/>
+
+![flex](../flex.png)
