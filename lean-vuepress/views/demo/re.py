fe100005687374cae8a29d7849268a4db60fc513
yanyangyang <yanyangyang@koopoo.com.cn>
2019-05-14 16:58:49 +0800

    init

--- lean-vuepress/views/demo/re.py
+++
@@ -1,9 +0,0 @@
-import re
-
-line = '//ww.baid.com'
-matchObj = re.search('com$',line, re.I|re.M)
-searchObj = re.search( r'(.*) are (.*?) .*', "Cats are smarter than dogs", re.M|re.I)
-subObj = re.sub('^x//','http://',line, 1,re.I|re.M)
-
-pass
-print(subObj)
