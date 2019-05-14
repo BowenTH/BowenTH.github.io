cd8f46c7c58b928cba69d814bfc2c1a20f3af986
yanyangyang <yanyangyang@koopoo.com.cn>
2019-05-14 18:50:00 +0800

    add

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,44 @@
 module.exports = {
-  title: 'bowenth的博客空间',
-  description:'个人博客空间',
-  base: '/page/dist/',
-  dest: 'docs/dist'
+  title: '日志',
+  description: '个人博客空间',
+  base: '/page/doc/',
+  dest: 'docs/doc',
 
+  themeConfig: {
+    nav: [{
+        text: '主页',
+        link: '/'
+      },
+      {
+        text: '博文',
+        items: [{
+            text: 'Android',
+            link: '/android/'
+          },
+          {
+            text: 'ios',
+            link: '/ios/'
+          },
+          {
+            text: 'Web',
+            link: '/web/'
+          }
+        ]
+      },
+      {
+        text: '关于',
+        link: '/about/'
+      },
+      {
+        text: 'Github',
+        link: 'https://www.github.com/codeteenager'
+      },
+    ],
+    sidebar:[
+      ['/', '首页'],
+      ['/demo/','训练场']
+    ],
+    sidebarDepth: 2,
+    lastUpdated: 'Last Updated',
+  }
 }
---
+++ docs/demo/README.md
@@ -0,0 +1,9 @@
+# demo 试验场
+
+> 测试一些属性
+
+## hello h2
+
+### hello h3
+
+#### hello h4
